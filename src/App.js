bd0474f58019dfab3385cbe496c1d8765416bfe7
aadeshjangra1408 <aadeshjangra2@gmail.com>
2022-10-20 21:33:39 +0530

    a

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import "../src/css/App.css";
 import "../src/css/Footer.css";
 import "../src/css/Root.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Menu from "./components/Menu";
 import Main from "./components/Main";
 import Testimonials from "./components/Testimonials";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Footer from "./components/Footer";
-import Pattey from "./components/Pattey";
+import Menupage from "./components/Menupage";
 // import { Route, Switch } from "react-router-dom";
 
 function App() {
@@ -22,8 +22,7 @@ function App() {
       <Main />
       <Testimonials />
       <Footer />
-      <Pattey />
-
+      <Menupage />
       {/* <Switch>
         <Route exact path="/" component={Hero} />
         <Route exact path="/About" component={About} />
---
+++ src/components/Menupage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Menupage = () => {
+  return <></>;
+};
+
+export default Menupage;
