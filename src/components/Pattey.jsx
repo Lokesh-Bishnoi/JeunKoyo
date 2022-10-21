dc6b4c2d87f1665b35c2493aa94f96dd0dfc1434
Lokesh Bishnoi <lokeshbishnoi527@gmail.com>
2022-10-21 16:18:57 +0530

    pattyy

--- src/components/Pattey.jsx
+++ src/components/Pattey.jsx
@@ -56,12 +56,12 @@ const Pattey = () => {
         <div className={`${navShow ? "nav-fix bg_orangee" : " fix-navbar"}`}>
           <div className="d-flex justify-content-center align-items-center flex-column text-center trans ff-Raleway">
             <Link to="/">
-              <a className="text-white fs-5 " href="#">
+              <a className="text-white fs-5" href="#">
                 Home
               </a>
             </Link>
-            <Link to="/Beffypatty">
-              <a className="text-white fs-5 mt-4 " href="#">
+            <Link className="mt-4" to="/Beffypatty">
+              <a className="text-white fs-5 " href="#">
                 Menu
               </a>
             </Link>
