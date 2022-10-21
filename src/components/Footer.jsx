1625a3e81ac31fb5d2e67bbf1b5779a50b7f41bf
Lokesh Bishnoi <lokeshbishnoi527@gmail.com>
2022-10-21 15:22:37 +0530

    dfew

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,39 +11,46 @@ const Footer = () => {
       <section className="bg_orange1 py-md-5">
         <div className="container py-4">
           <div className="row align-items-center justify-content-between mt-4">
-            <div className="col-6 col-lg-1">
+            <div className="col-6 col-lg-1 py-4">
               <Link to="/">
                 <a className="ff_raleway fw_semibold fs_md color_pink" href="#">
                   Home
                 </a>
               </Link>
             </div>
-            <div className="col-6 col-lg-2">
+            <div className="col-6 col-lg-2 py-4">
               <Link to="/Beffypatty">
                 <a className="ff_raleway fw_semibold fs_md color_pink" href="#">
                   Add to Cart
                 </a>
               </Link>
             </div>
-            <div className="col-lg-4">
+            <div className="col-lg-4 py-4">
               <img src={logo} alt="logo" className="w-100" />
               <p className="ff_raleway fw_regular fs_sm color_pink text-center">
-                Beef is one of the popular types of meat. Rich in high-quality protein, vitamins
+                Beef is one of the popular types of meat. Rich in high-quality
+                protein, vitamins
               </p>
-              <div className="d-flex justify-content-center align-items-center">
-                <img src={twitter} alt="twitter" />
-                <img src={fb} alt="fb" />
-                <img src={insta} alt="insta" />
+              <div className="d-flex justify-content-center align-items-center mt-4">
+                <a className="mx-3" href="#">
+                  <img src={twitter} alt="twitter" />
+                </a>
+                <a className="mx-3" href="#">
+                  <img src={fb} alt="fb" />
+                </a>
+                <a className="mx-3" href="#">
+                  <img src={insta} alt="insta" />
+                </a>
               </div>
             </div>
-            <div className="col-6 col-lg-2">
+            <div className="col-6 col-lg-2 py-4">
               <Link to="/Beffypatty">
                 <a className="ff_raleway fw_semibold fs_md color_pink" href="#">
                   Menu
                 </a>
               </Link>
             </div>
-            <div className="col-6 col-lg-2">
+            <div className="col-6 col-lg-2 py-4">
               <Link to="/Beffypatty">
                 <a className="ff_raleway fw_semibold fs_md color_pink" href="#">
                   Book Reservations
