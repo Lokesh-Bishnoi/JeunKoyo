4ae14f920e15e014453babaf78d3e76d0a712a13
aadeshjangra1408 <aadeshjangra2@gmail.com>
2022-10-21 14:20:55 +0530

    d

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import logo from "../images/svg/logo.svg";
 
 const Footer = () => {
   return (
     <>
       <section className="bg_orange1 py-md-5">
         <div className="container py-4">
-          <div className="row align-items-center justify-content -center">
-            <div className="col-lg-4">
-              <ul className="p-0">
+          <div className="row align-items-center justify-content-between">
+            <div className="col-lg-5">
+              <img src={logo} alt="logo" className="w-100" />
+              {/* <ul className="p-0">
                 <li className="mt-lg-3">
                   <Link to="/">
                     <a
@@ -46,10 +48,10 @@ const Footer = () => {
                     Book Reservations
                   </a></Link>
                 </li>
-              </ul>
+              </ul> */}
             </div>
-            <div className="col-lg-4">
-              <ul className="p-0">
+            <div className="col-lg-5">
+              <ul className="p-0 text-center">
                 <li className="mt-lg-3">
                   <Link to="/">
                     <a
@@ -62,34 +64,37 @@ const Footer = () => {
                 </li>
                 <li className="mt-lg-3">
                   <Link to="/Beffypatty">
-                  <a
-                    className="ff_raleway fw_semibold fs_md color_pink"
-                    href="#"
-                  >
-                    Menu
-                  </a></Link>
+                    <a
+                      className="ff_raleway fw_semibold fs_md color_pink"
+                      href="#"
+                    >
+                      Menu
+                    </a>
+                  </Link>
                 </li>
                 <li className="mt-lg-3">
                   <Link to="/Addtocart">
-                  <a
-                    className="ff_raleway fw_semibold fs_md color_pink"
-                    href="#"
-                  >
-                    Add to cart
-                  </a></Link>
+                    <a
+                      className="ff_raleway fw_semibold fs_md color_pink"
+                      href="#"
+                    >
+                      Add to cart
+                    </a>
+                  </Link>
                 </li>
                 <li className="mt-lg-3">
                   <Link to="/Reservations">
-                  <a
-                    className="ff_raleway fw_semibold fs_md color_pink"
-                    href="#"
-                  >
-                    Book Reservations
-                  </a></Link>
+                    <a
+                      className="ff_raleway fw_semibold fs_md color_pink"
+                      href="#"
+                    >
+                      Book Reservations
+                    </a>
+                  </Link>
                 </li>
               </ul>
             </div>
-            <div className="col-lg-4">
+            {/* <div className="col-lg-4">
               <ul className=" p-0">
                 <li className="mt-lg-3">
                   <Link to="/">
@@ -129,7 +134,7 @@ const Footer = () => {
                   </a></Link>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
